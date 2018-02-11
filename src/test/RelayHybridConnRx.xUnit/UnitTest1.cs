33a6af5686d90938738e89caad4909cdff9f0c0e
Jasper H. Bojsen <jasperhb@microsoft.com>
2018-02-11 10:38:45 +0100

    wip

---
+++ src/test/RelayHybridConnRx.xUnit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RelayHybridConnRx.xUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
