ec9ed330265e79575db78c6284979bb587b91145
Jasper H. Bojsen <jasperhb@microsoft.com>
2018-02-11 14:58:40 +0100

    Ready for release of v1.0.1

--- src/build/publishLocal.ps1
+++ src/build/publishLocal.ps1
@@ -1,12 +1,12 @@
 param([string]$betaver)
 
 if ([string]::IsNullOrEmpty($betaver)) {
-	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\HttpMachine.netstandard\bin\Release\netstandard1.0\HttpMachine.dll')).Version.ToString(3)
+	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\RelayHybridConnRx\bin\Release\netstandard2.0\IRelayHybridConnRx.dll')).Version.ToString(3)
 	}
 else {
-	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\HttpMachine.netstandard\bin\Release\netstandard1.0\HttpMachine.dll')).Version.ToString(3) + "-" + $betaver
+	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\RelayHybridConnRx\bin\Release\netstandard2.0\IRelayHybridConnRx.dll')).Version.ToString(3) + "-" + $betaver
 }
 
 .\build.ps1 $version
 
-nuget.exe push -Source "1iveowlNuGetRepo" -ApiKey key .\NuGet\HttpMachine.PCL.$version.symbols.nupkg
+nuget.exe push -Source "1iveowlNuGetRepo" -ApiKey key .\NuGet\RelayHybridConnRx.$version.symbols.nupkg
