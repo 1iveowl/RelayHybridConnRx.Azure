dc876b2ed255002b5a9a50e49439b96e270bf868
Jasper H. Bojsen <jasperhb@microsoft.com>
2018-02-12 18:09:41 +0100

    Added .NET Versions

--- src/build/build.ps1
+++ src/build/build.ps1
@@ -3,7 +3,10 @@ param([string]$version)
 if ([string]::IsNullOrEmpty($version)) {$version = "0.0.1"}
 
 $msbuild = join-path -path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin" -childpath "msbuild.exe"
+&$msbuild ..\interface\IRelayHybridConnRx\IRelayHybridConnRx.csproj /t:Build /p:Configuration="Release"
 &$msbuild ..\main\RelayHybridConnRx\RelayHybridConnRx.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\Crossplatform\RelayHybridConnRx.net45\RelayHybridConnRx.net45.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\Crossplatform\RelayHybridConnRx.net461\RelayHybridConnRx.net461.csproj /t:Build /p:Configuration="Release"
 
 
 
